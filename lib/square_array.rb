6c5866d8984f967a712c532abfbe9a8c59f08369
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-06 21:15:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0 
   new_array = []
   while counter < new_array.length do 
-    new_array.push(numbers[counter] ** 2)
+    new_array.push(numbers[counter] ** 1)
   
     counter += 1
   
